ad810131df24084054092994963782434643a7d4
Emanuel Leal <enmanuel.leal@sngular.com>
2022-08-10 11:17:38 +0200

    feature:
    - Change png to vector drawable

--- app/src/main/java/com/eeema/android/rickandmortyapp/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/eeema/android/rickandmortyapp/ui/detail/DetailScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -21,13 +20,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.compose.SubcomposeAsyncImage
+import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.eeema.android.data.model.Character
 import com.eeema.android.data.model.Gender
@@ -60,12 +60,12 @@ fun DetailContent(character: Character) {
         Column(
             modifier = Modifier.fillMaxSize().padding(vertical = 32.dp)
         ) {
-            SubcomposeAsyncImage(
+            AsyncImage(
                 model = ImageRequest.Builder(LocalContext.current)
                     .data(character.image)
                     .crossfade(true)
                     .build(),
-                loading = { CircularProgressIndicator() },
+                placeholder = painterResource(R.drawable.ic_brand),
                 contentDescription = stringResource(R.string.characters_image_content_description),
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
